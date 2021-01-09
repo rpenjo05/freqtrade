bdcf1de4c5ec5365df68dd1df8ea8365315a93f2
rpenjo05 <49451716+rpenjo05@users.noreply.github.com>
2021-01-10 00:16:15 +0530

    Update combine_opt.py

--- user_data/hyperopts/combine_opt.py
+++ user_data/hyperopts/combine_opt.py
@@ -92,6 +92,33 @@ class CombinedBinHAndClucOpt(IHyperOpt):
 		roi_table[params['roi_t10'] + params['roi_t9'] + params['roi_t8'] + params['roi_t7'] + params['roi_t6'] + params['roi_t5'] + params['roi_t4'] + params['roi_t3'] + params['roi_t2'] + params['roi_t1']] = 0
 
 		return roi_table
+
+	@staticmethod
+	def roi_space() -> List[Dimension]:
+
+		return [
+		    Integer(1, 300, name='roi_t10'),
+		    Integer(1, 300, name='roi_t9'),
+		    Integer(1, 300, name='roi_t8'),
+		    Integer(1, 300, name='roi_t7'),
+		    Integer(1, 300, name='roi_t6'),
+		    Integer(1, 300, name='roi_t5'),
+		    Integer(1, 300, name='roi_t4'),
+		    Integer(1, 300, name='roi_t3'),
+		    Integer(1, 300, name='roi_t2'),
+		    Integer(1, 300, name='roi_t1'),
+
+		    Real(0.001, 0.005, name='roi_p10'),
+		    Real(0.001, 0.005, name='roi_p9'),
+		    Real(0.001, 0.005, name='roi_p8'),
+		    Real(0.001, 0.005, name='roi_p7'),
+		    Real(0.001, 0.005, name='roi_p6'),
+		    Real(0.001, 0.005, name='roi_p5'),
+		    Real(0.001, 0.005, name='roi_p4'),
+		    Real(0.001, 0.005, name='roi_p3'),
+		    Real(0.0001, 0.005, name='roi_p2'),
+		    Real(0.0001, 0.005, name='roi_p1'),
+		]
 		
 	@staticmethod
 	def sell_strategy_generator(params: Dict[str, Any]) -> Callable:
