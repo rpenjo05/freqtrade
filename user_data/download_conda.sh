8e562954991833efad7d142439b6736705cab86f
rpenjo05 <49451716+rpenjo05@users.noreply.github.com>
2021-04-13 21:12:53 +0530

    Create download_conda.sh

---
+++ user_data/download_conda.sh
@@ -0,0 +1,3 @@
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+wget https://raw.githubusercontent.com/rpenjo05/freqtrade/develop/user_data/install_freqtrade.sh
+sh Anaconda3-2020.11-Linux-x86_64.sh
