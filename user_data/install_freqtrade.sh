202674463572b6decebd0bb5a12aa657865e0319
rpenjo05 <49451716+rpenjo05@users.noreply.github.com>
2021-06-29 18:17:50 +0530

    Update install_freqtrade.sh

--- user_data/install_freqtrade.sh
+++ user_data/install_freqtrade.sh
@@ -1,6 +1,7 @@
 conda create -n freqtrade python=3.8
 conda activate freqtrade
-git clone --single-branch --branch stable https://github.com/freqtrade/freqtrade.git
+git clone https://github.com/freqtrade/freqtrade.git
+#git clone --single-branch --branch stable https://github.com/freqtrade/freqtrade.git
 cd freqtrade
 chmod 777 setup.sh
 ./setup.sh --install
