25aafca743865203e5c562dac163d9e056596f6f
rpenjo05 <49451716+rpenjo05@users.noreply.github.com>
2021-07-17 17:29:43 +0530

    Update CombinedBinHAndCluc.py

--- user_data/strategies/CombinedBinHAndCluc.py
+++ user_data/strategies/CombinedBinHAndCluc.py
@@ -207,31 +207,31 @@ class CombinedBinHAndCluc(IStrategy):
 
 	def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 		dataframe.loc[
-		    (  # strategy BinHV45
-			    dataframe['lower'].shift().gt(0) &
-			    dataframe['bbdelta'].gt(dataframe['close'] * 0.008) &
-			    dataframe['closedelta'].gt(dataframe['close'] * 0.0175) &
-			    dataframe['tail'].lt(dataframe['bbdelta'] * 0.25) &
-			    dataframe['close'].lt(dataframe['lower'].shift()) &
-			    dataframe['close'].le(dataframe['close'].shift())
-		    ) |
-		    (  # strategy ClucMay72018
-			    (dataframe['close'] < dataframe['ema_slow']) &
-			    (dataframe['close'] < 0.985 * dataframe['bb_lowerband']) &
-			    (dataframe['volume'] < (dataframe['volume_mean_slow'].shift(1) * 20))
-		    ),
-		    'buy'
+			(  # strategy BinHV45
+				dataframe['lower'].shift().gt(0) &
+				dataframe['bbdelta'].gt(dataframe['close'] * 0.008) &
+				dataframe['closedelta'].gt(dataframe['close'] * 0.0175) &
+				dataframe['tail'].lt(dataframe['bbdelta'] * 0.25) &
+				dataframe['close'].lt(dataframe['lower'].shift()) &
+				dataframe['close'].le(dataframe['close'].shift())
+			) |
+			(  # strategy ClucMay72018
+				(dataframe['close'] < dataframe['ema_slow']) &
+				(dataframe['close'] < 0.985 * dataframe['bb_lowerband']) &
+				(dataframe['volume'] < (dataframe['volume_mean_slow'].shift(1) * 20))
+			),
+			'buy'
 		] = 1
 		return dataframe
 
-    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        """
-        """
-        dataframe.loc[
-        	(qtpylib.crossed_below(dataframe['sell-rsi'], 83) ) &
-            (dataframe['close'] > dataframe['bb_middleband2'])
-            
-            ,
-            'sell'
-        ] = 1
-        return dataframe
+	def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+		"""
+		"""
+		dataframe.loc[
+			(qtpylib.crossed_below(dataframe['sell-rsi'], 83) ) &
+			(dataframe['close'] > dataframe['bb_middleband2'])
+			
+			,
+			'sell'
+		] = 1
+		return dataframe
